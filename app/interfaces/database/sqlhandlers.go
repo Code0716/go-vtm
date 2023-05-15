3d934c478badc5980e6d6918099f17db0661dc1b
code0716 <job_info@code0716.com>
2023-05-15 21:36:20 +0900

    small fix

--- app/interfaces/database/sqlhandlers.go
+++ app/interfaces/database/sqlhandlers.go
@@ -10,9 +10,6 @@ type SQLHandlerInterface interface {
 	Create(value any) db.SQLHandler
 	Find(value any, where ...any) db.SQLHandler
 	First(value any, where ...any) db.SQLHandler
-	IsExist(tableName string, query any, args ...any) (bool, error)
-	GetAllAdminUsers(params domain.Pager) ([]*domain.AdminUser, int64, error)
-	AdminMemberGetAll(params domain.Pager) ([]*domain.Member, int64, error)
 	Save(value any) db.SQLHandler
 	Delete(value any, where ...any) db.SQLHandler
 	Where(query any, args ...any) db.SQLHandler
@@ -21,4 +18,8 @@ type SQLHandlerInterface interface {
 	Having(query any, args ...any) db.SQLHandler
 	Pluck(column string, dest any) db.SQLHandler
 	Error() error
+	// TODO:下記は今後なくす予定
+	IsExist(tableName string, query any, args ...any) (bool, error)
+	GetAllAdminUsers(params domain.Pager) ([]*domain.AdminUser, int64, error)
+	AdminMemberGetAll(params domain.Pager) ([]*domain.Member, int64, error)
 }
